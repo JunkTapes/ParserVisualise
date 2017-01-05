519cc2d740156a64bd74b94fa06e5757237685ac
SadPyro <Mikevictim@gmail.com>
2017-01-06 02:35:17 +0300

    Частично отказался от массивов в пользу ArrayList, добавил dependencies

--- ParserVisualise/src/application/EventHandler.java
+++ ParserVisualise/src/application/EventHandler.java
@@ -28,12 +28,8 @@ public class EventHandler {
 		GridPane gridPane = addGridPane();
 		Pane.getChildren().add(gridPane);
 
-		LoadSentence(InputTextArea,gridPane);
-		
-		/*if((words[0].getLayoutX()+words[1].getLayoutX())<70){
-			Pane.setPrefWidth((Pane.getWidth()+((70-(words[0].getLayoutX()+words[1].getLayoutX()))*NumberOfWords)));
-		}*/
-		
+		SentenceLoader.LoadSentence(InputTextArea,gridPane);
+
 	}
 	
 	public GridPane addGridPane() {
@@ -47,54 +43,7 @@ public class EventHandler {
 	}
 	
 	//ƒанные всегда правильные, их провер€ть не нужно
-	private void LoadSentence(TextArea inputTextArea2, GridPane gridPane) {
-		String input = inputTextArea2.getText();
-		String[] splited = input.split("\\s+");
-		String[] justwords= new String[(splited.length/3 )+ 1];
-		String[] justedges= new String[(splited.length/3 )+ 1];
-		String[] justedges2= new String[(splited.length/3 )+ 1];
-		int j=0;
-		for (int i = 0; i<splited.length; i+=3)
-		{
-			justwords[j]=splited[i];
-			j++;
-		}	
-		j=0;
-		for(int i=1;i<splited.length;i+=3)
-		{
-			justedges[j]=splited[i];
-			j++;
-		}
-		j=0;
-		for(int i=1;i<splited.length;i+=3)
-		{
-			justedges2[j]=splited[i];
-			j++;
-		}
-				
-		Label[] words = new Label[justwords.length];
-		Label[] edges= new Label [justedges.length];
-		for (int i = 0; i <= justwords.length-1; i++) {
-			 words[i] = new Label();
-			 edges[i] = new Label();
-			 words[i].setText(justwords[i]);
-			 edges[i].setText(justedges[i]);
-			}
-		
-		gridPane.addRow(1, words);
-		gridPane.addRow(2, edges);
-//		hbox.getChildren().addAll(words);
-//		hboxedges.setLayoutY(120);
-//		hboxedges.getChildren().addAll(edges);
-		
-		
-		for(int i=0;i<justwords.length;i++)
-		{	
-			System.out.println(justwords[i]);
-		}
-				
-	}
-
+	
 	public void setMainApp(MyApp Interface) {
 
 		this.Interface = Interface;
--- ParserVisualise/src/application/SentenceLoader.java
+++ ParserVisualise/src/application/SentenceLoader.java
@@ -1,12 +1,56 @@
 package application;
 import java.util.*;
 import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
 public class SentenceLoader {
 
-	public void LoadSentence(TextArea inputTextArea2) {
+	static void LoadSentence(TextArea inputTextArea2, GridPane gridPane) {
+		//нужна отдельная функция по записи текста в arrayListы
+		String input = inputTextArea2.getText();
+		String[] splited = input.split("\\s+");
+		ArrayList<String> tokens= new ArrayList<String>();
+		ArrayList<String> edges= new ArrayList<String>();
+		ArrayList<String> fromDependency= new ArrayList<String>();
+		ArrayList<String> dependencies= new ArrayList<String>();
 		
+		for (int i=0; i<splited.length;i+=4)
+			tokens.add(splited[i]);		
+		for(int i=1; i<splited.length;i+=4)
+			edges.add(splited[i]);
+		for(int i=2; i<splited.length;i+=4)
+			fromDependency.add(splited[i]);
+		for(int i=3; i<splited.length;i+=4)
+			dependencies.add(splited[i]);
+			
+		//нужно уничтожать все уже нарисованное в случае повторного нажатия клавиши
 		
+		Label[] labelTokens = new Label[tokens.size()];
+		Label[] labelEdges= new Label [edges.size()];
+		Label[] labelFromDependency = new Label [fromDependency.size()];
+		Label[] labeldependencies = new Label [dependencies.size()];
+		for (int i = 0; i <= tokens.size()-1; i++) {
+			 labelTokens[i] = new Label();
+			 labelEdges[i] = new Label();
+			 labelFromDependency[i]= new Label();
+			 labeldependencies[i]= new Label();
+			 labelTokens[i].setText(tokens.get(i));
+			 labelEdges[i].setText(edges.get(i));
+			 labelFromDependency[i].setText(fromDependency.get(i));
+			 labeldependencies[i].setText(dependencies.get(i));
+			}
+		
+		gridPane.addRow(1, labelTokens);
+		gridPane.addRow(2, labelEdges);
+		gridPane.addRow(3,labelFromDependency);
+		gridPane.addRow(4,labeldependencies);
+
+		for(int i=0;i<tokens.size();i++)
+		{	
+			System.out.println(tokens.get(i));
+		}
+				
 	}
+
 
 }
